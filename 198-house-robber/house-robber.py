5cbd487dfc654718430fc0a450b6339099d3f01a
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2025-12-09 10:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.19%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        def robbery(i,dp:dict):
+        '''def robbery(i,dp:dict):
             if i == n:
                 return 0
             if i == n-1:
@@ -12,4 +12,12 @@ class Solution:
 
                 dp[i] = max(rob,skip)
             return dp[i]
-        return robbery(0,{})
+        return robbery(0,{})'''
+        if n <= 2:
+            return max(nums)
+        dp = [0]*n
+        dp[0],dp[1] = nums[0],nums[1]
+        dp[2]=nums[2]+dp[0]
+        for i in range(3,n):
+            dp[i]=max(dp[i-2],dp[i-3])+nums[i]
+        return max(dp[-2:])
