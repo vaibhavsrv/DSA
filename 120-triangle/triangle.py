8b1735f20baffb575634e9cc39ea940215288366
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2025-12-11 11:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (76.90%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,13 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        '''dp = triangle[-1][:]
+        dp = triangle[-1][:]
         for i in range(n-2,-1,-1):
             for j in range(i+1):
                 dp[j] = triangle[i][j] + min(dp[j],dp[j+1])
 
-        return dp[0]'''
-        for i in range(n-2,-1,-1):
+        return dp[0]
+        '''for i in range(n-2,-1,-1):
             for j in range(len(triangle[i])):
                 triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1])
-        return triangle[0][0]
+        return triangle[0][0]'''
