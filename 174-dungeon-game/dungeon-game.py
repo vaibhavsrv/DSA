26c30f014b50cb7d0d14036782f8ce3ad279fcc1
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2025-12-17 10:02:35 +0530

    Time: 7 ms (58.19%) | Memory: 18.9 MB (55.96%) - LeetSync

--- 174-dungeon-game/dungeon-game.py
+++ 174-dungeon-game/dungeon-game.py
@@ -1,43 +1,10 @@
 class Solution:
-    def calculateMinimumHP(self, dungeon):
-        m = len(dungeon)
-        n = len(dungeon[0])
-
-        def solve(minHealth):
-            dp = [[-1] * n for _ in range(m)]
-
-            dp[0][0] = minHealth + dungeon[0][0]
-            if dp[0][0] <= 0:
-                return False
-
-            for i in range(m):
-                for j in range(n):
-                    if dp[i][j] <= 0:
-                        continue
-
-                    if i + 1 < m:
-                        dp[i + 1][j] = max(
-                            dp[i + 1][j],
-                            dp[i][j] + dungeon[i + 1][j]
-                        )
-
-                    if j + 1 < n:
-                        dp[i][j + 1] = max(
-                            dp[i][j + 1],
-                            dp[i][j] + dungeon[i][j + 1]
-                        )
-
-            return dp[m - 1][n - 1] > 0
-
-        left, right = 1, 10**7
-        answer = right
-
-        while left <= right:
-            mid = (left + right) // 2
-            if solve(mid):
-                answer = mid
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return answer
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m,n = len(dungeon),len(dungeon[0])
+        dp = [float('inf')] * (n+1)
+        dp[n - 1] = 1
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                take = min(dp[j],dp[j+1])-dungeon[i][j]
+                dp[j] = max(1,take)
+        return dp[0]
