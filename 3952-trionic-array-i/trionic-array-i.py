37160cbf0721fabdf9b167b60b6566bd3db678e4
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-02-03 21:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.11%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isTrionic(self, nums: List[int], k: int = 3) -> bool:
+        if nums[0] >= nums[1]:
+            return False
+
+        n = len(nums)
+        sign, switches = 1, 0
+        for i in range(1, n):
+            delta = nums[i] - nums[i - 1]
+            if delta == 0:
+                return False
+            elif delta * sign < 0:
+                sign *= -1
+                switches += 1
+
+        return switches == k - 1
