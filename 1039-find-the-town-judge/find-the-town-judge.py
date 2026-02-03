ec17013863ad4f59e44d42aa41868fba4698cd41
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-02-03 15:34:07 +0530

    Time: 11 ms (74.22%) | Memory: 22.7 MB (20.46%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,6 +1,6 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if len(trust) == 0 and n == 1:
+        '''if len(trust) == 0 and n == 1:
             return 1
         count = [0] * (n+1)
         for i,j in trust:
@@ -9,4 +9,15 @@ class Solution:
         for p,c in enumerate(count):
             if c == n - 1 :
                 return p
+        return -1'''
+        indegree = [0]*(n+1)
+        outdegree = [0]*(n+1)
+
+        for a,b in trust:
+            indegree[b]+=1
+            outdegree[a]+=1
+        
+        for i in range(1,n+1):
+            if indegree[i] == n-1 and outdegree[i]==0:
+                return i
         return -1
