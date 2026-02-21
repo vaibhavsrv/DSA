aa3493931c9c7ef6d5e433b942326b6d233479be
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-02-21 13:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.89%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        result = []
+        for i in range(1,target[-1]+1):
+            result.append("Push")
+            if i not in target:
+                result.append("Pop")
+        return result
