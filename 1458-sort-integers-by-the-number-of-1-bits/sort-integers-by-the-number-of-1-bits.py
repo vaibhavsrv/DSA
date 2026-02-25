5113e1793965e7a6519857c36a5637ba24cd1a5d
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-02-25 09:58:27 +0530

    Time: 7 ms (41.60%) | Memory: 19.4 MB (44.54%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
+        # Brute Force solution of O(nlogn), it works because the constraints is only 500
         temp = []
         for num in arr:
             temp.append((bin(num).count('1'),num))
@@ -8,3 +9,6 @@ class Solution:
         for item in temp:
             answer.append(item[1])
         return answer
+
+        #Optimal can be something like we can sort with key pair and use lambda function
+        return sorted(arr,key=lambda x:bin(x).count('1'))
