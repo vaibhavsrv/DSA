bf50c59914179c1429e2f50dab6c5ed0e49f2ca7
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-02-26 11:57:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (80.78%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -2,8 +2,9 @@ class Solution:
     def numSteps(self, s: str) -> int:
         result = 0
         carry = False
+        # the loop will be start from end->till it find 0, in reverse order
         for i in s[:0:-1]:
-            if i == '1' and not carry or i == '0' and carry:
+            if i == '1' and not carry or i == '0' and carry: # if this possible then add 2 to the result
                 result += 2
                 carry = True
             else:
