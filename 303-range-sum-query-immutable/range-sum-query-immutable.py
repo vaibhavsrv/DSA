6d342a47fbe1c380a9bd1265dead7136895b3780
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-03-09 11:22:24 +0530

    Time: 3 ms (75.04%) | Memory: 22.9 MB (29.65%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -1,14 +1,24 @@
+''' #This is the brute force solution where it takes O(Q*N) -> order of Q times N #
 class NumArray:
-
     def __init__(self, nums: List[int]):
         self.nums = nums
-
     def sumRange(self, left: int, right: int) -> int:
         count = 0
         for i in range(left,right+1):
             count += self.nums[i]
-        return count
-
+        return count'''
+# The optimal can be like->
+   # we can pre compute the value of the array and whenever required just return the value
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.prefix = [0] * len(nums)
+        self.prefix[0] = nums[0]
+        for i in range(1,len(nums)):
+            self.prefix[i] = self.prefix[i-1]+nums[i]
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.prefix[right]
+        return self.prefix[right] - self.prefix[left - 1]             
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
