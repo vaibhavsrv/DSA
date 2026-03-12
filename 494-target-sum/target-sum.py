cc4d52fa960956758c6bcf18e3851a5bfb265048
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-03-12 14:47:02 +0530

    Time: 183 ms (19.93%) | Memory: 74.3 MB (14.26%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # THE IS THE RECURSIVE CODE FOR THIS
         memo = {}
         def dp(i,current_sum):
             if i == len(nums):
                 return 1 if current_sum == target else 0
-            if (i,current_sum)in memo: # THIS LINE IS FOR MEMOIZATION
-                return memo[(i,current_sum)] # RETURN THE MOMO
-            memo[(i,current_sum)] = dp(i+1,current_sum + nums[i]) + dp(i+1,current_sum - nums[i]) # CALLING RECURSION HERE
+            if (i,current_sum) in memo:
+                return memo[(i,current_sum)]
+            memo[(i,current_sum)] = dp(i+1,current_sum + nums[i]) + dp(i+1,current_sum - nums[i])
             return memo[(i,current_sum)]
-        return dp(0,0)# RECURSION BASE CASE
+        return dp(0,0)
