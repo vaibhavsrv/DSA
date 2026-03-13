c0534cb27dcf3bd3b93b8138b464f611e3a53bc8
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-03-13 12:42:52 +0530

    Time: 3 ms (25.04%) | Memory: 19.6 MB (14.45%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,17 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-
-        def dp(index, memo: dict):
-            if index == n:
+        memo = {}
+        def dp(i):
+            if i == n:
                 return 1
-            if s[index] == "0":
+            if i in memo:
+                return memo[i]
+            if s[i] == '0':
                 return 0
-            if index not in memo:
-                memo[index] = 0
-                if 1 <= int(s[index]) <= 9:
-                    memo[index] += dp(index+1, memo)
-                if 10 <= int(s[index:index+2]) <= 26:
-                    memo[index] += dp(index+2, memo)
-            return memo[index]
-        return dp(0, {})
+            memo[i] = dp(i+1)
+
+            if i+1 < n and int(s[i:i+2]) <= 26:
+                memo[i] += dp(i+2)
+            return memo[i]
+        return dp(0)
