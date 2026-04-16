d8ae4393d20f034ae649dd95831715e6a0528626
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-04-16 21:35:28 +0530

    Time: 292 ms (78.91%) | Memory: 55.2 MB (26.56%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        n = len(nums)
+
+        indices = defaultdict(list)
+        for i, num in enumerate(nums):
+            indices[num].append(i)
+
+        result = []
+        for q in queries:
+            qind = indices[nums[q]]
+            if len(qind) == 1:
+                result.append(-1)
+            else:
+                j = bisect_left(qind, q)
+                prev = qind[(j - 1) % len(qind)]
+                nextt = qind[(j + 1) % len(qind)]
+                result.append(min(
+                    (nextt + n - qind[j]) % n,
+                    (qind[j] - prev + n) % n,
+                ))
+        return result
