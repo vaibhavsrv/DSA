a03e4ca43835be45aa8f9b5589e9f30056e6f73e
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-05-16 16:42:35 +0530

    Time: 27 ms (47.67%) | Memory: 21.4 MB (89.68%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        count = 0
+
+        for i in range(0,len(nums),2):
+            count += nums[i]
+
+        return count
