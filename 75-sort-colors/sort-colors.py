e0a3815c02393a1b4e5530123cf0137385070c3b
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-06-03 11:28:02 +0530

    Time: 3 ms (10.39%) | Memory: 19.4 MB (22.42%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)-1):
-            if nums[i] >= nums[i+1]:
-                nums[i],nums[i+1] = nums[i+1],nums[i]
-        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if nums[i] >= nums[j]:
+                    nums[i],nums[j] = nums[j],nums[i]
