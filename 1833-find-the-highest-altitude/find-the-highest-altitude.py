5e370453ee08d2bddd014485949fabd96d634a04
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-06-19 20:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.40%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -1,10 +1,8 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         sum = [0]
-
         for i in gain:
-            a = sum[len(sum) - 1]
-            b = i + a
-
+            a=sum[len(sum)-1]
+            b=i+a
             sum.append(b)
         return max(sum)
