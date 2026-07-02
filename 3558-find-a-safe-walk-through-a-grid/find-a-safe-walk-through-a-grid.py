7f54393ec5c892017264c1c2920ee586c789498c
Vaibhav Srivastava <srvvaibhav8@gmail.com>
2026-07-02 23:50:26 +0530

    Time: 55 ms (91.04%) | Memory: 19.4 MB (97.17%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,27 +1,34 @@
-from heapq import heappush, heappop
-from math import inf
-from typing import List
-
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
-        m, n = len(grid),len(grid[0])
-        dist = [[inf] * n for _ in range(m)]
+        m, n = len(grid), len(grid[0])
+
+        INF = float('inf')
+        dist = [[INF] * n for _ in range(m)]
+
+        dq = deque()
         dist[0][0] = grid[0][0]
-        heap = [(grid[0][0],0,0)]
-        directions =[(1,0),(-1,0),(0,1),(0,-1)]
-        while heap:
-            cost,x,y = heappop(heap)
-            if cost > dist[x][y]:
-                continue
+        dq.appendleft((0, 0))
 
-            if (x, y) == (m - 1, n - 1):
-                return True
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-            for dx,dy in directions:
-                nx, ny = x + dx,y + dy
+        while dq:
+            x, y = dq.popleft()
+
+            if x == m - 1 and y == n - 1:
+                return dist[x][y] < health
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+
                 if 0 <= nx < m and 0 <= ny < n:
-                    new_cost = cost + grid[nx][ny]
-                    if new_cost < dist[nx][ny] and new_cost < health:
-                        dist[nx][ny] = new_cost
-                        heappush(heap, (new_cost,nx,ny))
-        return False
+                    w = grid[nx][ny]
+
+                    if dist[x][y] + w < dist[nx][ny]:
+                        dist[nx][ny] = dist[x][y] + w
+
+                        if w == 0:
+                            dq.appendleft((nx, ny))
+                        else:
+                            dq.append((nx, ny))
+
+        return dist[m - 1][n - 1] < health
